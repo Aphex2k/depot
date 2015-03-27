aa7be9753f6bd164cffd73d70e2ceee596cfce3b
kkoch <kkoch@trustinternational.com>
2015-03-27 17:01:50 +0100

    s 128

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if(session[:indexCounter].nil?)
+      session[:indexCounter] = 0;
+    else
+      session[:indexCounter] +=  1;
+    end
+
+    @index_counter_message = "Page was called #{session[:indexCounter]} times" if session[:indexCounter] > 5
   end
 end
